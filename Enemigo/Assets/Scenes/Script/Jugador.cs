007f3f7bd36d3ff9cd0e65bcb64fab3e11f1d5d7
Celtor123 <celsosan11@gmail.com>
2019-12-01 21:56:00 +0100

    Master
    Primer Commit.

---
+++ Enemigo/Assets/Scenes/Script/Jugador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+	// Use this for initialization
+	public GameObject player;
+	public float velocidad;
+	void Start () {
+		velocidad=10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+           float movhorizontal=Input.GetAxis("Horizontal");
+        float movvertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movhorizontal,0.0f,movvertical);
+        Rigidbody rb=player.GetComponent<Rigidbody>();
+		rb.AddForce(movimiento * velocidad);
+		
+		
+	}
+}
---
+++ Enemigo/Assets/Scenes/Script/Movimiento.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+    public UnityEngine.AI.NavMeshAgent agent;
+	// Use this for initialization
+	public GameObject fin;
+	void Start () {
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+		
+		Transform a= fin.GetComponent<Transform>();
+		agent.SetDestination(a.position);
+	}
+}
